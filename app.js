ae7f1aae37c60a6014502dec0a9a39f38b5069ef
David Fertitta <davidj.fertitta@gmail.com>
2021-08-11 20:32:36 -0500

    vue code added for attacks

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
-const app = Vue.createApp({});
+
+function getRandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+const app = Vue.createApp({
+    data() {
+      return {
+        playerHealth: 100,
+        monsterHealth: 100 
+      };
+    },
+    methods: {
+        attackMonster() {
+            const attackValue = getRandomNumber(5, 12);
+            this.monsterHealth -= attackValue;
+            this.attackPlayer();
+        },
+        attackPlayer() {
+            const attackValue = getRandomNumber(8, 15);
+            this.playerHealth -= attackValue;
+        }
+    }
+});
 
 app.mount('#game');
