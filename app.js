294f6f903ae6af180353709e2fe1c86b07c03e0e
David Fertitta <davidj.fertitta@gmail.com>
2021-08-16 22:00:07 -0500

    surrender function added. play options visible once game has ended

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ const app = Vue.createApp({
             this.winner = null;
             this.currentRound = 0;
         },
+        surrender() {
+           this.winner = 'monster'; 
+        }
     },
 });
 
