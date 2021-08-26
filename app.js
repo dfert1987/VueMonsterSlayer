c6aeba1e0bd10b37ec631c5bcbe69f826582d8c4
David Fertitta <davidj.fertitta@gmail.com>
2021-08-26 14:30:01 -0500

    enemy characters added. pics added. random number function added.

--- app.js
+++ app.js
@@ -14,21 +14,21 @@ const app = Vue.createApp({
       logMessages: [],
       attacksAvailable: true,
       chars: [
-      {
-        name: '???',
-        class: '???',
-        attack: '',
-        special: '',
-        heal: '',
-        image: './question.jpeg'
-      },
+        {
+          name: '???',
+          class: '???',
+          attack: '',
+          special: '',
+          heal: '',
+          image: './images/question.jpeg',
+        },
         {
           name: 'Kindergarten Teacher',
           class: 'LaoWai',
           attack: 'Bu Yao!',
           special: 'Play Hangman!',
           heal: 'Drink Beer!',
-          image: './laowai.png'
+          image: './images/laowai.png',
         },
         {
           name: 'Dada DJ',
@@ -36,7 +36,7 @@ const app = Vue.createApp({
           attack: 'Bathroom Bump!',
           special: 'Drum n Bass!',
           heal: 'Do Shot!',
-          image: './dada.jpeg'
+          image: './images/dada.jpeg',
         },
         {
           name: 'Wide Body',
@@ -44,7 +44,7 @@ const app = Vue.createApp({
           attack: 'Poop!',
           special: 'Spelling Snakes!',
           heal: 'Big Mac For No Reason!',
-          image: './wb.png'
+          image: './images/wb.png',
         },
         {
           name: 'Nigerian Bro',
@@ -52,23 +52,23 @@ const app = Vue.createApp({
           attack: 'Sup Bro, you good?',
           special: 'Bag',
           heal: 'Re-up',
-          image: './richy.jpeg'
+          image: './images/richy.jpeg',
         },
         {
           name: 'Water Man',
-          class: 'Delivery',
+          class: 'Driver',
           attack: 'Jug Drop Off!',
           special: 'Wreckless Driving!',
           heal: 'Refill',
-          image: './water.jpeg'
+          image: './images/water.jpeg',
         },
         {
           name: 'Jinshisong',
-          class: 'Delivery',
+          class: 'Driver',
           attack: 'Delivery Fee!',
           special: 'Wrong Order!',
           heal: 'Gets Paid!',
-          image: './jinshisong.jpeg'
+          image: './images/jinshisong.jpeg',
         },
         {
           name: 'JianBing Queen',
@@ -76,7 +76,7 @@ const app = Vue.createApp({
           attack: 'Scallions!',
           special: 'La Jiao!',
           heal: 'Weixin Zhifu!',
-          image: './jian.jpg'
+          image: './images/jian.jpg',
         },
       ],
       char: {
@@ -85,9 +85,76 @@ const app = Vue.createApp({
         attack: '',
         special: '',
         heal: '',
-        image: './question.jpeg'
+        image: './images/question.jpeg',
       },
+      enemies: [
+        {
+          name: '???',
+          class: '???',
+          attack: '',
+          special: '',
+          heal: '',
+          image: './images/question.jpeg',
+        },
+        {
+          name: 'Taxi Shifu',
+          class: 'Driver',
+          attack: 'Honk!',
+          special: 'Zai Nar!',
+          heal: 'Cigarette Break!',
+          image: './images/taxi.jpeg',
+        },
+        {
+          name: 'Squatter',
+          class: 'Beijinger',
+          attack: 'Smoke',
+          special: 'Loogie',
+          heal: 'Phone game!',
+          image: './images/taxi.jpeg',
+        },
+        {
+          name: 'Yellow Weasel',
+          class: 'Animal',
+          attack: 'Scurry',
+          special: 'Scavange',
+          heal: 'Eats trash!',
+          image: './images/weasel.jpeg',
+        },
+        {
+          name: 'Houh Hai Swimmer',
+          class: 'Beijinger',
+          attack: 'Splash',
+          special: 'Breast Stroke',
+          heal: 'Dry off!',
+          image: './images/swimmer.jpeg',
+        },
+        {
+          name: 'Sleepy BaoAn',
+          class: 'Authority',
+          attack: 'Doze!',
+          special: 'Snore!',
+          heal: 'Dream!',
+          image: './images/baoan.jpeg',
+        },
+        {
+          name: 'Beijing Bikini',
+          class: 'LaoRen',
+          attack: 'Belly rub!',
+          special: 'Baijiu Burp!',
+          heal: 'Rolls Shirt!',
+          image: './images/baoan.jpeg',
+        },
+      ],
       index: 0,
+      enemyIndex: 0,
+      enemy: {
+        name: '???',
+        class: '???',
+        attack: '',
+        special: '',
+        heal: '',
+        image: './images/question.jpeg',
+      },
     };
   },
   computed: {
@@ -217,7 +284,12 @@ const app = Vue.createApp({
         this.index++;
       }
     },
-    getPlayerName() {},
+    getRandomNumber() {
+      this.enemyIndex = Math.floor(
+        Math.random() * (this.enemies.length - 2) + 1
+      );
+      console.log(this.enemyIndex);
+    },
   },
 });
 
